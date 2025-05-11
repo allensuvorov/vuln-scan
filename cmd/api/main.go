ad66baa5fd39844670a2704e18c0e15eac46195a
Allen <allenvs@mail.ru>
2025-05-11 13:23:25 -0700

    Use standard logger.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -10,11 +11,10 @@ func main() {
 		fmt.Fprintln(w, "OK")
 	})
 
-	fmt.Println("Starting server on :8080")
+	log.Print("starting server on :8080")
+
 	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+	log.Fatal(err)
 }
 
 // This is a simple HTTP server that responds to health check requests.
